b2b12f99f9c70a409badce6f5055babcc91a4bd9
hachinrayhan <rayhan.hachin@gmail.com>
2022-09-03 08:54:48 +0600

    hover added on categories

--- js/app.js
+++ js/app.js
@@ -4,14 +4,19 @@ const loadCategories = () => {
         .then(data => displayCategories(data.data.news_category))
 }
 
-const displayCategories = (categories) => {
+const displayCategories = categories => {
     const categoryUl = document.getElementById('category-ul');
     categories.forEach(category => {
         const categoryItem = document.createElement('li');
         categoryItem.classList.add('list-group-item', 'border-0');
+        categoryItem.setAttribute('onclick', `loadNews(${category.category_id})`)
         categoryItem.innerText = category.category_name;
         categoryUl.appendChild(categoryItem);
     })
+}
+
+const loadNews = catId => {
+    console.log(catId);
 }
 
 loadCategories();
