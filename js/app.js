e85045519a6db95a9f74786e297039ecf943df84
hachinrayhan <rayhan.hachin@gmail.com>
2022-09-03 16:03:30 +0600

    title, news, author and view added

--- js/app.js
+++ js/app.js
@@ -31,29 +31,26 @@ const displayNews = news => {
         const singleNewsDiv = document.createElement('div');
         singleNewsDiv.classList.add('col');
         singleNewsDiv.innerHTML = `
-        <div class="card mb-3">
+        <div class="card mb-3 p-3">
             <div class="row g-0">
                 <div class="col-md-4">
                     <img src="${singleNews.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural
-                            lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">${singleNews.title.slice(0, 50)}...</h5>
+                        <p class="card-text">${singleNews.details.slice(0, 80)}...</p>
                         <div class="d-flex justify-content-between align-items-center">
-                            <div class="d-flex">
+                            <div class="d-flex align-items-center">                               
+                                <img class="img-fluid rounded-circle w-25 me-2" src="${singleNews.author.img}" alt="">                               
                                 <div>
-                                    <img src="..." alt="">
-                                </div>
-                                <div>
-                                <h4>Jane Cooper</h4>
-                                <p>Jan 10, 2022</p>
+                                    <h6>${singleNews.author.name}</h6>
+                                    <p class="m-0">${singleNews.author.published_date}</p>
                                 </div>
                             </div>
-                            <div>
-                                <i class="fa-regular fa-eye"></i>
-                                <span>1.5M</span>
+                            <div class="d-flex align-items-center">
+                                <i class="fa-regular fa-eye me-2"></i>
+                                <span>${singleNews.total_view}</span>
                             </div>
                         </div>
                     </div>
